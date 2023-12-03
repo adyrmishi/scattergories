302261137278e40a844accd9b270bb6ffbb89d27
adyrmishi <angeladyrmishi@outlook.com>
2023-12-03 16:56:04 +0000

    use deployed site url

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const port = 3001;
 app.use(cookieParser());
 app.use(sessionMiddleware);
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3001/', 'http://localhost:3000/');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3001/', 'https://scattergories.onrender.com/');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
